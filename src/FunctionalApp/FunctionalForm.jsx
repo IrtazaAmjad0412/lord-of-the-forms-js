76b1f55739210125ea7c487d82459c2b319c72e7
IrtazaAmjad0412 <irtaza0412@gmail.com>
2025-10-30 19:52:51 -0400

    Added focus functionality to the phone inputs

--- src/FunctionalApp/FunctionalForm.jsx
+++ src/FunctionalApp/FunctionalForm.jsx
@@ -1,59 +1,136 @@
+import { useState, useRef } from "react";
 import { ErrorMessage } from "../ErrorMessage";
+import { FunctionalTextInput } from "./FunctionalTextInput";
+import { FunctionalPhoneInput } from "./FunctionalPhoneInput";
 
 const firstNameErrorMessage = "First name must be at least 2 characters long";
 const lastNameErrorMessage = "Last name must be at least 2 characters long";
 const emailErrorMessage = "Email is Invalid";
-const cityErrorMessage = "State is Invalid";
+const cityErrorMessage = "City is Invalid";
 const phoneNumberErrorMessage = "Invalid Phone Number";
 
 export const FunctionalForm = () => {
+  const [firstNameInput, setFirstNameInput] = useState("");
+  const [lastNameInput, setLastNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [cityInput, setCityInput] = useState("");
+  const [phoneNumberState, setPhoneNumberState] = useState(["", "", "", ""]);
+
+  const refs = [useRef(), useRef(), useRef(), useRef()];
+
+  const reset = () => {
+    setFirstNameInput("");
+    setLastNameInput("");
+    setEmailInput("");
+    setCityInput("");
+    setPhoneNumberState(["", "", "", ""]);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      firstNameInput,
+      lastNameInput,
+      emailInput,
+      cityInput,
+      phoneNumberState,
+    });
+    reset();
+  };
+
+  const handlePhoneOnChange = (index) => (e) => {
+    const lengths = [2, 2, 2, 1];
+    const currentMaxLength = lengths[index];
+    const nextRef = refs[index + 1];
+    const prevRef = refs[index - 1];
+    const value = e.target.value;
+
+    const shouldGoToNextRef = currentMaxLength <= value.length && nextRef;
+    const shouldGoToPrevRef = value.length === 0 && prevRef;
+
+    const newPhoneNumberState = phoneNumberState.map(
+      (phoneNumberInput, phoneNumberIndex) =>
+        index === phoneNumberIndex ? value : phoneNumberInput
+    );
+    if (shouldGoToNextRef) {
+      nextRef.current?.focus();
+    }
+    if (shouldGoToPrevRef) {
+      prevRef.current?.focus();
+    }
+    setPhoneNumberState(newPhoneNumberState);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
       <u>
         <h3>User Information Form</h3>
       </u>
 
-      {/* first name input */}
-      <div className="input-wrap">
-        <label>{"First Name"}:</label>
-        <input placeholder="Bilbo" />
-      </div>
+      <FunctionalTextInput
+        label={"First Name"}
+        inputProps={{
+          type: "text",
+          placeholder: "Bilbo",
+          onChange: (e) => {
+            setFirstNameInput(e.target.value);
+          },
+          value: firstNameInput,
+        }}
+      />
       <ErrorMessage message={firstNameErrorMessage} show={true} />
 
-      {/* last name input */}
-      <div className="input-wrap">
-        <label>{"Last Name"}:</label>
-        <input placeholder="Baggins" />
-      </div>
+      <FunctionalTextInput
+        label={"Last Name"}
+        inputProps={{
+          type: "text",
+          placeholder: "Baggins",
+          onChange: (e) => {
+            setLastNameInput(e.target.value);
+          },
+          value: lastNameInput,
+        }}
+      />
       <ErrorMessage message={lastNameErrorMessage} show={true} />
 
-      {/* Email Input */}
-      <div className="input-wrap">
-        <label>{"Email"}:</label>
-        <input placeholder="bilbo-baggins@adventurehobbits.net" />
-      </div>
+      <FunctionalTextInput
+        label={"Email"}
+        inputProps={{
+          type: "email",
+          placeholder: "bilbo-baggins@adventurehobbits.net",
+          onChange: (e) => {
+            setEmailInput(e.target.value);
+          },
+          value: emailInput,
+        }}
+      />
       <ErrorMessage message={emailErrorMessage} show={true} />
 
-      {/* City Input */}
-      <div className="input-wrap">
-        <label>{"City"}:</label>
-        <input placeholder="Hobbiton" />
-      </div>
+      <FunctionalTextInput
+        label={"City"}
+        inputProps={{
+          type: "text",
+          placeholder: "Hobbiton",
+          onChange: (e) => {
+            setCityInput(e.target.value);
+          },
+          value: cityInput,
+        }}
+      />
       <ErrorMessage message={cityErrorMessage} show={true} />
 
-      <div className="input-wrap">
-        <label htmlFor="phone">Phone:</label>
-        <div id="phone-input-wrap">
-          <input type="text" id="phone-input-1" placeholder="55" />
-          -
-          <input type="text" id="phone-input-2" placeholder="55" />
-          -
-          <input type="text" id="phone-input-3" placeholder="55" />
-          -
-          <input type="text" id="phone-input-4" placeholder="5" />
-        </div>
-      </div>
-
+      <FunctionalPhoneInput
+        htmlFor={"phone"}
+        label={"Phone"}
+        refs={refs}
+        handlePhoneOnChange={handlePhoneOnChange}
+        phoneNumberState={phoneNumberState}
+        inputProps={{
+          type: "tel",
+          placeholder: "55",
+          maxLength: 2,
+        }}
+      />
       <ErrorMessage message={phoneNumberErrorMessage} show={true} />
 
       <input type="submit" value="Submit" />
---
+++ src/FunctionalApp/FunctionalPhoneInput.jsx
@@ -0,0 +1,49 @@
+export const FunctionalPhoneInput = ({
+  htmlFor,
+  label,
+  refs,
+  handlePhoneOnChange,
+  phoneNumberState,
+  inputProps,
+}) => {
+  return (
+    <div className="input-wrap">
+      <label htmlFor={htmlFor}>{label}:</label>
+      <div id="phone-input-wrap">
+        <input
+          id="phone-input-1"
+          ref={refs[0]}
+          onChange={handlePhoneOnChange(0)}
+          value={phoneNumberState[0]}
+          {...inputProps}
+        />
+        -
+        <input
+          id="phone-input-2"
+          ref={refs[1]}
+          onChange={handlePhoneOnChange(1)}
+          value={phoneNumberState[1]}
+          {...inputProps}
+        />
+        -
+        <input
+          id="phone-input-3"
+          ref={refs[2]}
+          onChange={handlePhoneOnChange(2)}
+          value={phoneNumberState[2]}
+          {...inputProps}
+        />
+        -
+        <input
+          id="phone-input-4"
+          ref={refs[3]}
+          onChange={handlePhoneOnChange(3)}
+          value={phoneNumberState[3]}
+          {...inputProps}
+          placeholder="5"
+          maxLength={1}
+        />
+      </div>
+    </div>
+  );
+};

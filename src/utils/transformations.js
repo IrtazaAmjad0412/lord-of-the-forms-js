293e14449d5993b252d65f076408de4001e78692
IrtazaAmjad0412 <irtaza0412@gmail.com>
2025-10-31 15:18:15 -0400

    Made functions to capitalize first, last name and format the phone number

--- src/utils/transformations.js
+++ src/utils/transformations.js
@@ -1,9 +1,19 @@
-export const capitalize = () => {
-    // todo: build this function
-    // `capitalize("jOn")` should output `"Jon"`
-}
+export const capitalize = (str) => {
+  // todo: build this function
+  // `capitalize("jOn")` should output `"Jon"`
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
+};
 
-export const formatPhoneNumber = () => {
-    // todo: build this function
-    // `formatPhoneNumber("1234567")` should be `"12-34-56-7"`
-}
+export const formatPhoneNumber = (arr) => {
+  // todo: build this function
+  // `formatPhoneNumber("1234567")` should be `"12-34-56-7"`
+  const str = arr.join("");
+  let formattedStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (i % 2 === 0 && i !== 0) {
+      formattedStr += "-";
+    }
+    formattedStr += str[i];
+  }
+  return formattedStr;
+};

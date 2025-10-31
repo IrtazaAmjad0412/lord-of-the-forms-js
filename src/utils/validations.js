1516f8acad48f14ac2bba8c9ec352599dac11131
IrtazaAmjad0412 <irtaza0412@gmail.com>
2025-10-30 21:32:39 -0400

    Added functions to help validate form inputs

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,5 +1,15 @@
+import { allCities } from "./all-cities";
+
 export function isEmailValid(emailAddress) {
   // eslint-disable-next-line no-useless-escape
   const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return !!emailAddress.match(regex);
+}
+
+export function isCityValid(cityPara) {
+  return allCities.some((city) => city.toLowerCase() === cityPara.toLowerCase());
+}
+
+export function isPhoneValid(phoneNum) {
+  return /^\d{7}$/.test(phoneNum.join(""));
 }
